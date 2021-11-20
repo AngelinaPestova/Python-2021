b2b3b12f4b2baa303352afd8e17db7ff7b45f7fd
Angelina_Pestova <gelyapestova@gmail.com>
2021-11-21 03:52:02 +0500

    Steps 9-11

--- filter.py
+++ filter.py
@@ -1,37 +1,66 @@
 import argparse
+import doctest
 
 import numpy as np
 from PIL import Image
 
 
-def get_block_sum(start_y, start_x, arr, block_size):
-    block_sum = 0
-    for row in range(start_y, start_y + block_size):
-        for col in range(start_x, start_x + block_size):
-            red = int(arr[row][col][0])
-            green = int(arr[row][col][1])
-            blue = int(arr[row][col][2])
-            block_sum += (red + green + blue) / 3
-    block_sum = int(block_sum // block_size ** 2)
-    return block_sum
+def get_block_mean(arr, block_size):
+    """
+    Возвращает среднее значение сумм цветов пикселей для заданного участка
+
+    Параметры:
+    :param arr: Массив участка
+    :param block_size: Размер блока
+    :return: Среднее значение цвета пикселей для заданного участка
+
+    >>> get_block_mean([[[1, 1, 3]]], 1)
+    1
+    >>> get_block_mean([[[1, 2, 3]]], 1)
+    2
+    >>> get_block_mean([[[0, 0, 0]]], 1)
+    0
+    >>> get_block_mean([[[5, 4, 3]] * 3] * 3, 3)
+    4
+    """
+    return int(np.sum(arr) / 3 // (block_size ** 2))
 
 
-def make_block_gradation(start_y, start_x, arr, block_sum, block_size, gradations_amount):
+def make_block_gradation(start_y, start_x, arr, block_mean, block_size, gradations_amount):
+    """
+    Возвращает массив с измененным цветом пикселей у заданного участка
+
+    :param start_y: y-координата левого нижнего пикселя участка
+    :param start_x: x-координата левого нижнего пикселя участка
+    :param arr: Исходный массив
+    :param block_mean: Среднее значение цвета пикселей для заданного участка
+    :param block_size: Размер блока
+    :param gradations_amount: Количество градаций серого
+    :return: Массив с измененным цветом у заданного участка
+    """
+    color = int(block_mean // gradations_amount) * gradations_amount
     for row in range(start_y, start_y + block_size):
         for col in range(start_x, start_x + block_size):
-            arr[row][col][0] = int(block_sum // gradations_amount) * gradations_amount
-            arr[row][col][1] = int(block_sum // gradations_amount) * gradations_amount
-            arr[row][col][2] = int(block_sum // gradations_amount) * gradations_amount
+            arr[row][col] = [color, color, color]
+    return arr
 
 
 def transform_image(img, block_size, gradation_amount):
+    """
+    Возвращает изображение с наложенным фильтром
+
+    :param img: Исходное изображение
+    :param block_size: Размер блока
+    :param gradation_amount: Количество градаций серого
+    :return: Изображение с наложенным фильтром
+    """
     arr = np.array(img)
     height = len(arr)
     width = len(arr[1])
     for y in range(0, height - 1, block_size):
         for x in range(0, width - 1, block_size):
-            block_sum = get_block_sum(y, x, arr, block_size)
-            make_block_gradation(y, x, arr, block_sum, block_size, gradation_amount)
+            block_mean = get_block_mean(arr[y:y + block_size, x:x + block_size], block_size)
+            arr = make_block_gradation(y, x, arr, block_mean, block_size, gradation_amount)
     return Image.fromarray(arr)
 
 
@@ -47,7 +76,8 @@ def main():
     gradation_amount = args.gradation_amount
 
     res = transform_image(img, block_size, gradation_amount)
-    res.save("result_image.jpg")
+    res.save("res_filter.jpg")
 
 
+print(doctest.testmod())
 main()
