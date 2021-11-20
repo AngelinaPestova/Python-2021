9f59711c2d749ec53955559c4b57767187dc69b5
Angelina_Pestova <gelyapestova@gmail.com>
2021-11-21 04:29:38 +0500

    Code checked for PEP8 requirements

--- filter.py
+++ filter.py
@@ -26,7 +26,10 @@ def get_block_mean(arr, block_size):
     return int(np.sum(arr) / 3 // (block_size ** 2))
 
 
-def make_block_gradation(start_y, start_x, arr, block_mean, block_size, gradations_amount):
+def make_block_gradation(
+    start_y, start_x, arr,
+    block_mean, block_size, gradations_amount
+):
     """
     Возвращает массив с измененным цветом пикселей у заданного участка
 
@@ -59,8 +62,14 @@ def transform_image(img, block_size, gradation_amount):
     width = len(arr[1])
     for y in range(0, height - 1, block_size):
         for x in range(0, width - 1, block_size):
-            block_mean = get_block_mean(arr[y:y + block_size, x:x + block_size], block_size)
-            arr = make_block_gradation(y, x, arr, block_mean, block_size, gradation_amount)
+            block_mean = get_block_mean(
+                arr[y:y + block_size, x:x + block_size],
+                block_size
+            )
+            arr = make_block_gradation(
+                y, x, arr, block_mean,
+                block_size, gradation_amount
+            )
     return Image.fromarray(arr)
 
 
