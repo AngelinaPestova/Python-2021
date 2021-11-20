2d3cf63764cedd6a15542b15e577dbe72e80592f
Angelina_Pestova <gelyapestova@gmail.com>
2021-11-21 04:41:40 +0500

    Code checked for PEP8 requirements

--- filter_with_filename.py
+++ filter_with_filename.py
@@ -1,27 +1,20 @@
-import argparse
-
 import numpy as np
 from PIL import Image
 
 
-def get_block_sum(start_y, start_x, arr, block_size):
-    block_sum = 0
-    for row in range(start_y, start_y + block_size):
-        for col in range(start_x, start_x + block_size):
-            red = int(arr[row][col][0])
-            green = int(arr[row][col][1])
-            blue = int(arr[row][col][2])
-            block_sum += (red + green + blue) / 3
-    block_sum = int(block_sum // block_size ** 2)
-    return block_sum
+def get_block_mean(arr, block_size):
+    return int(np.sum(arr) / 3 // (block_size ** 2))
 
 
-def make_block_gradation(start_y, start_x, arr, block_sum, block_size, gradations_amount):
+def make_block_gradation(
+    start_y, start_x, arr,
+    block_mean, block_size, gradations_amount
+):
+    color = int(block_mean // gradations_amount) * gradations_amount
     for row in range(start_y, start_y + block_size):
         for col in range(start_x, start_x + block_size):
-            arr[row][col][0] = int(block_sum // gradations_amount) * gradations_amount
-            arr[row][col][1] = int(block_sum // gradations_amount) * gradations_amount
-            arr[row][col][2] = int(block_sum // gradations_amount) * gradations_amount
+            arr[row][col] = [color, color, color]
+    return arr
 
 
 def transform_image(img, block_size, gradation_amount):
@@ -30,15 +23,21 @@ def transform_image(img, block_size, gradation_amount):
     width = len(arr[1])
     for y in range(0, height - 1, block_size):
         for x in range(0, width - 1, block_size):
-            block_sum = get_block_sum(y, x, arr, block_size)
-            make_block_gradation(y, x, arr, block_sum, block_size, gradation_amount)
+            block_mean = get_block_mean(
+                arr[y:y + block_size, x:x + block_size],
+                block_size
+            )
+            arr = make_block_gradation(
+                y, x, arr, block_mean,
+                block_size, gradation_amount
+            )
     return Image.fromarray(arr)
 
 
 def main():
     img = Image.open("img2.jpg")
     res = transform_image(img, 10, 50)
-    res.save("result_image.jpg")
+    res.save("res_filter_with_filename.jpg")
 
 
 main()
